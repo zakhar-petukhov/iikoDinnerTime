b2fa815400ec92951044884a625b90f462bb7344
zakhar-petukhov <zakharpetukhov@protonmail.com>
2020-12-02 01:53:17 +0300

    :card_file_box: update on_detele field

--- dinner_time/apps/api/users/models.py
+++ dinner_time/apps/api/users/models.py
@@ -16,7 +16,7 @@ class CustomGroup(Model):
     company = ForeignKey('company.Company', on_delete=PROTECT, related_name='groups_company', blank=True,
                          null=True, verbose_name='Компания')
 
-    address_for_delivery = ForeignKey('company.DeliveryAddress', on_delete=PROTECT,
+    address_for_delivery = ForeignKey('company.DeliveryAddress', on_delete=CASCADE,
                                       related_name='group_delivery_address',
                                       blank=True, null=True, verbose_name='Адрес доставки')
 
